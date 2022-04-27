8a3a27686afab4b6ec97ecb3433014af6fd273a3
Sandro Castro <castrosandro2@gmail.com>
2022-04-27 00:16:56 -0300

    te

--- app/Http/Controllers/MasterController.php
+++ app/Http/Controllers/MasterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\EnderecoMaster;
 use App\Models\Master;
+use App\Models\Iten;
 
 class MasterController extends Controller
 {
--- resources/views/master/alocar.blade.php
+++ resources/views/master/alocar.blade.php
@@ -277,6 +277,14 @@
 @stop
 
 
+@section('footer')      
+    <strong>Copyright © 2014-2022 <a href="https://goeyewear.com.br/">GO Eyewear</a>.</strong>
+    Todos direitos reservados.
+    <div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> 1.0.0
+    </div>
+@stop
+
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
 @stop
