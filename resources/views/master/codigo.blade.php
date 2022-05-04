4a1d71a70bb8c0a061a6730bf4b04c6402e3d957
sandro.castro <sandro.castro@goeyewear.com.br>
2022-05-04 15:28:34 -0300

    ds

--- resources/views/master/codigo.blade.php
+++ resources/views/master/codigo.blade.php
@@ -49,12 +49,13 @@
                     @method('POST')
                     @csrf
 
-                    <label for="Nome">Código</label>     
-                    <input style="display: inline;" type="password" class="form-control" name="codigo" id="senha" placeholder="Digite o Código" required>
+                    <div class="input-group">
+                        <label for="Nome">Código</label>     
+                        <input type="password" class="form-control" name="codigo" id="senha" placeholder="Digite o Código" required>
 
-                    <i id="olhoa" onclick="show()" class="far fa-eye"></i>
-                    <i id="olhof" onclick="show()" class="far fa-eye-slash"  style="display: none;"></i>
-
+                        <i id="olhoa" onclick="show()" class="far fa-eye"></i>
+                        <i id="olhof" onclick="show()" class="far fa-eye-slash"  style="display: none;"></i>
+                    </div>
                     <br><br>
                     <button type="submit" class="btn btn-primary">Atualizar</button>
                 </form>
